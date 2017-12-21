26d558167b76f748a59b283930efb692884c6f94
navicester <navicester@qq.com>
2017-12-21 16:43:30 +0800

    update

--- reimbursement/reimbursement/forms.py
+++ reimbursement/reimbursement/forms.py
@@ -1,8 +1,19 @@
 from django import forms
-from .models import InvoiceImage
+from .models import InvoiceImage, Invoice
 
 class InvoiceImageForm(forms.ModelForm):
-  #image = forms.ImageField()
-  class Meta:
-    model = InvoiceImage
-    fields = ['image']
+    class Meta:
+        model = InvoiceImage
+        fields = ['image']
+
+class InvoiceForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(InvoiceForm, self).__init__(*args, **kwargs)
+        # self.fields['start'].widget.attrs['class'] ="calenda"
+        # self.fields['end'].widget.attrs['class'] ="calenda"
+
+    class Meta:
+      model = Invoice
+      exclude = ['']    
+
