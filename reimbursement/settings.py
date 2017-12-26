13394f4472f5622eb9bbfbd48758c630341eec46
navicester <navicester@qq.com>
2017-12-26 21:16:21 +0800

    update

--- reimbursement/settings.py
+++ reimbursement/settings.py
@@ -128,6 +128,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static_in_pro", "our_static"),
     os.path.join(BASE_DIR, "static_in_pro", "weui"),
+    os.path.join(BASE_DIR, "static_in_pro", "wechat"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "static_in_env", "static_root")
 MEDIA_ROOT = os.path.join(BASE_DIR, "static_in_env", "media_root")
